b8330f3522bebfef458fe466a9c161f53e0e39a7
Code4Hacker <black2022mail@gmail.com>
2023-03-09 20:29:03 +0300

    Changing Big Navigation file

--- src/components/BigNavigator.jsx
+++ src/components/BigNavigator.jsx
@@ -20,7 +20,7 @@ const BigNavigator = ({ smash}) => {
                 <div className="list">
                     <ul>
                         <li><Link to="/" className='link'>Home</Link></li>
-                        <li><Link to="about" className='link'>About</Link></li>
+                        <li><Link to="about" className='link'>About </Link></li>
                         <li><Link to="blog" className='link'>Blog</Link></li>
                     </ul>
                 </div>
